19396902062c38500bccea3e2f42e8ee3c0aa867
Anny Herath <annyherath@gmai.com>
2025-07-31 08:31:00 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (_req, res) => {
+  res.send('Hello from Express + TypeScript!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
